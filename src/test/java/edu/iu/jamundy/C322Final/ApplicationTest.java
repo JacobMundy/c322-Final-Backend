3d0c3c652936348af7d2c28e1c9d70ec9c1c5f4d
jmund <jmundy2003@gmail.com>
2024-05-02 00:45:57 -0400

    Added test file

---
+++ src/test/java/edu/iu/jamundy/C322Final/ApplicationTest.java
@@ -0,0 +1,11 @@
+package edu.iu.jamundy.C322Final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
